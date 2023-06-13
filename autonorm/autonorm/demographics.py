2e700f9cf4ccf956b8f6378d3da669b573c560de
Matt Doiron <mjdoiron@gmail.com>
2023-06-13 17:17:49 -0400

    Initial commit6

--- autonorm/autonorm/demographics.py
+++ autonorm/autonorm/demographics.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from src.enums import Race, Sex, Handedness, DemographicVariableType, DemographicCoding
+from .enums import Race, Sex, Handedness, DemographicVariableType, DemographicCoding
 
 
 class DemographicVariable():
