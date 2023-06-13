1666ac7cab1a2b1322c619a59ba687f4eb39b933
Matt Doiron <mjdoiron@gmail.com>
2023-06-13 17:39:58 -0400

    Initial commit12

--- autonorm/setup.py
+++ autonorm/setup.py
@@ -1,7 +1,4 @@
-from setuptools import setup
-
-setup(
-    name='autonorm',
-    version='0.1',
-    packages=['demographics'],
-)
+from distutils.core import setup
+setup(name='autonorm',
+      version='1.0',
+      )
