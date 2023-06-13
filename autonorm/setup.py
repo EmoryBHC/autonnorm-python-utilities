7e9ec587363e662abd981af711749b78d4308883
Matt Doiron <mjdoiron@gmail.com>
2023-06-13 17:43:58 -0400

    Initial commit13

--- autonorm/setup.py
+++ autonorm/setup.py
@@ -1,4 +1,12 @@
-from distutils.core import setup
-setup(name='autonorm',
-      version='1.0',
-      )
+from setuptools import setup
+
+setup(
+    name="autonorm",
+    version="1.0.0",
+    description="A Python module for autonorm",
+    author="Your Name",
+    packages=["autonorm"],
+    install_requires=[
+        # List any dependencies your module requires
+    ],
+)
