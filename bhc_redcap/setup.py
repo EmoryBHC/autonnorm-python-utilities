a223efbd101c9d3d73e8e257fcfbf600184f49fb
Matt Doiron <mjdoiron@gmail.com>
2023-06-14 13:00:45 -0400

    Trying multiple modules9

--- bhc_redcap/setup.py
+++ bhc_redcap/setup.py
@@ -5,8 +5,6 @@ setup(
     version="1.0.0",
     description="A Python module for autonorm",
     author="Your Name",
-    packages=["autonorm"],
-    install_requires=[
-        # List any dependencies your module requires
-    ],
+    packages=["bhc_redcap"],
+    install_requires=[],
 )
---
+++ bhc_utilities/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="bhc_utilities",
+    version="1.0.0",
+    description="A Python module for autonorm",
+    author="Your Name",
+    packages=["bhc_utilities"],
+    install_requires=[],
+)
