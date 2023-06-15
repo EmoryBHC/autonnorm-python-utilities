3ce119fdcdc045f40ccf19c8a2e01dae7f5ddc01
Matt Doiron <mjdoiron@gmail.com>
2023-06-15 16:36:07 -0400

    Trying to condense import statements

--- bhc_psyncs/__init_.py
+++ bhc_psyncs/__init_.py
@@ -1,1 +1,6 @@
- 
+from .bhc_psyncs.controller import *
+from .bhc_psyncs.enums import *
+from .bhc_psyncs.demographics import *
+from .bhc_psyncs.normalized_scores import *
+from .bhc_psyncs.score import *
+from .bhc_psyncs.psyncs_norm_response import *
