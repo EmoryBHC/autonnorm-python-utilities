e1003d0a6c3af67a33709b857e7eb8a6de1bf46f
Matt Doiron <mjdoiron@gmail.com>
2023-06-15 14:57:10 -0400

    Added tests dir for all

---
+++ tests/bhc_psyncs/test_psyncs_controller.py
@@ -0,0 +1,7 @@
+from bhc_psyncs.bhc_psyncs.controller import PsyncsController
+
+
+def test_controller():
+    b = PsyncsController()
+    a = True
+    assert a
---
+++ tests/bhc_redcap/test_redcap_controller.py
@@ -0,0 +1,7 @@
+from bhc_redcap.bhc_redcap.controller import RedcapController
+
+
+def test_controller():
+    b = RedcapController()
+    a = True
+    assert a
