9084ce7b7b4c82395f692fbb139aac8570bfdbd8
Matt Doiron <mjdoiron@gmail.com>
2023-06-23 11:18:54 -0400

    Removed api_version var from lambda invocation

--- bhc_aws/bhc_aws/lambda_invocation.py
+++ bhc_aws/bhc_aws/lambda_invocation.py
@@ -4,12 +4,11 @@ from .lambda_request_context import LambdaRequestContext
 
 
 class LambdaInvocation:
-    def __init__(self, event: dict, context: dict, api_version: str):
+    def __init__(self, event: dict, context: dict):
         self.request_context: LambdaRequestContext = LambdaRequestContext(event['requestContext'])
         self.body: Optional[dict] = self.get_request_body(event)
         self.path: str = event["path"]
         self.context: dict = context
-        self.api_version: str = api_version
 
     def get_request_body(self, event: dict):
         request_body = event.get("body")
